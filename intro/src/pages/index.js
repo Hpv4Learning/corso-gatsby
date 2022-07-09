cd1a765a7eaf48bb270079ee8aca1eb180c9d66c
Omar De Angelis <omardeangelis@o.deangelis>
2022-07-09 12:17:49 +0200

    styled-components

--- intro/src/pages/index.js
+++ intro/src/pages/index.js
@@ -1,17 +1,32 @@
 import React from "react"
-import { Layout } from "../components"
+import styled from "styled-components"
+import { Button, Layout, CustomButton } from "../components"
 //Far vede dall'insepect come le classi vengono nominate
-import { page, text } from "../modules/home.module.css"
+
+const Wrapper = styled.div`
+  color: #4b4b4b;
+
+  h1 {
+    color: #341268;
+  }
+
+  .text {
+    text-transform: capitalize;
+  }
+`
+
 export default function Home() {
   return (
     <Layout>
-      <div className={page}>
+      <Wrapper>
         <h1>Sono la home pages</h1>
-        <p className={text}>
+        <p className="text">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil sed ab
           sunt optio illum at aut sit ea esse quisquam?
         </p>
-      </div>
+        <Button>Sono un bottone</Button>
+        <CustomButton isFullWidth>Eccomi</CustomButton>
+      </Wrapper>
     </Layout>
   )
 }
