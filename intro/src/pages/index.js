50252d9b9ec69fc8680f0cda25cb99c419caca9d
Omar De Angelis <omardeangelis@o.deangelis>
2022-07-09 10:58:18 +0200

    link navigation

--- intro/src/pages/index.js
+++ intro/src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
-
+import { Link } from "gatsby"
 export default function Home() {
-  return <div>Sono la Home Page</div>
+  return (
+    <div>
+      <h1>Sono la home pages</h1>
+      <nav
+        style={{
+          display: "flex",
+          gap: "4px",
+        }}
+      >
+        <Link to="/about/">About</Link>
+        <Link to="/shop/">Shop</Link>
+        <a href="/contact/">Contatti</a>
+      </nav>
+    </div>
+  )
 }
--- intro/src/pages/shop/index.js
+++ intro/src/pages/shop/index.js
@@ -1,7 +1,24 @@
 import React from "react"
-
+import { Link } from "gatsby"
 const ShopHome = () => {
-  return <div>ShopHome</div>
+  return (
+    <div>
+      <h1>ShopHome</h1>
+      <nav
+        style={{
+          display: "flex",
+          gap: "4px",
+        }}
+      >
+        <Link to="/">Home</Link>
+        {/* Path Relativo */}
+        <Link to="shoes">Shoes</Link>
+        {/* Path assoluto */}
+        {/* <Link to="/socks/">Socks</Link> */}
+        <Link to="/shop/socks/">Socks</Link>
+      </nav>
+    </div>
+  )
 }
 
 export default ShopHome
