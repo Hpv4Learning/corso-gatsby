f5d00d65884879e935944ae0316b818ac58a363e
Omar De Angelis <omardeangelis@o.deangelis>
2022-07-09 11:59:05 +0200

    css-modules

--- intro/src/pages/about.js
+++ intro/src/pages/about.js
@@ -1,8 +1,19 @@
 import React from "react"
 import { Layout } from "../components"
+import * as aboutStyle from "../modules/about.module.css"
 
 const about = () => {
-  return <Layout>Benvenuto nell'about</Layout>
+  return (
+    <Layout>
+      <div className={aboutStyle.page}>
+        <h1>About Page</h1>
+        <p className={aboutStyle.text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil sed ab
+          sunt optio illum at aut sit ea esse quisquam?
+        </p>
+      </div>
+    </Layout>
+  )
 }
 
 export default about
--- intro/src/pages/index.js
+++ intro/src/pages/index.js
@@ -1,9 +1,17 @@
 import React from "react"
 import { Layout } from "../components"
+//Far vede dall'insepect come le classi vengono nominate
+import { page, text } from "../modules/home.module.css"
 export default function Home() {
   return (
     <Layout>
-      <h1>Sono la home pages</h1>
+      <div className={page}>
+        <h1>Sono la home pages</h1>
+        <p className={text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil sed ab
+          sunt optio illum at aut sit ea esse quisquam?
+        </p>
+      </div>
     </Layout>
   )
 }
