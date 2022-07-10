a129fb14d057e24cd0badfeca0a9471b4b0a612f
Omar De Angelis <omardeangelis@o.deangelis>
2022-07-10 22:33:48 +0200

    versione furba

--- intro/src/components/StaticQuery.js
+++ intro/src/components/StaticQuery.js
@@ -1,43 +1,23 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
+//Regola, va usato in un componente con la prima lettera maiuscola o in un custom hook
 export const StaticQuery = () => {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          title
-          description
-          author
-          dummyData
-          type {
-            category
-            level
-            score
-          }
-          otherCourses {
-            category
-            level
-            score
-          }
-        }
-      }
-      allSitePage {
-        nodes {
-          path
-        }
-      }
-    }
-  `)
+  const {
+    site: {
+      siteMetadata: { title, otherCourses },
+    },
+  } = useStaticQuery(query)
+
   return (
     <div className="spacer">
       <h4>Scopri il nostro corso</h4>
-      <b>{data.site.siteMetadata.title}</b>
+      <b>{title}</b>
       <div className="spacer">
         <p> Altri corsi </p>
         <div className="spacer">
           <ul>
-            {data.site.siteMetadata.otherCourses.map(course => {
+            {otherCourses.map(course => {
               return <li key={course.category}>{course.category}</li>
             })}
           </ul>
@@ -46,3 +26,31 @@ export const StaticQuery = () => {
     </div>
   )
 }
+
+const query = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        dummyData
+        type {
+          category
+          level
+          score
+        }
+        otherCourses {
+          category
+          level
+          score
+        }
+      }
+    }
+    allSitePage {
+      nodes {
+        path
+      }
+    }
+  }
+`
