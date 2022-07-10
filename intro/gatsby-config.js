890877cd0181b39e6956d4fafc6af858aec837c5
Omar De Angelis <omardeangelis@o.deangelis>
2022-07-10 22:08:20 +0200

    compile siteMetedata

--- intro/gatsby-config.js
+++ intro/gatsby-config.js
@@ -6,6 +6,29 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Introduzione a Gatsby",
+    description: "La vera sfida è scrivere giusto Gatsby",
+    author: "@mariocemento",
+    type: {
+      category: "SSR",
+      level: "intermediate",
+      score: 7,
+    },
+    dummyData: ["item 1", "item 2"],
+    otherCourses: [
+      {
+        category: "React",
+        level: "junior",
+        score: 7,
+      },
+      {
+        category: "Javascript",
+        level: "basic",
+        score: 7,
+      },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
