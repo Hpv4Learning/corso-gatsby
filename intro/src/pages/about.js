70db8e6de2dd3803f5f5c4d23a4600be953131aa
Omar De Angelis <omardeangelis@o.deangelis>
2022-07-10 20:12:59 +0200

    useGastbyImage

--- intro/src/pages/about.js
+++ intro/src/pages/about.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Layout } from "../components"
 import styled from "styled-components"
 import image from "../assets/images/big-image.jpeg"
+import { StaticImage } from "gatsby-plugin-image"
 
 const ImageBox = styled.div({
   maxWidth: "343px",
@@ -25,9 +26,16 @@ const about = () => {
         }}
       >
         <h3>Images</h3>
-        <ImageBox className="spacer">
-          <img src={image} />
-        </ImageBox>
+        <div className="spacer">
+          <ImageBox>
+            <StaticImage src="../assets/images/big-image.jpeg" />
+          </ImageBox>
+        </div>
+        <div className="spacer">
+          <ImageBox>
+            <StaticImage src="https://i.pinimg.com/564x/67/6b/4b/676b4b47db6cc1d6e25429858e1a8b2e.jpg" />
+          </ImageBox>
+        </div>
       </div>
       <div className="spacer"></div>
     </Layout>
