16a13247248cad8b37065a220aa2d4476958a134
Omar De Angelis <omardeangelis@o.deangelis>
2022-07-10 21:50:39 +0200

    all image layout

--- intro/src/pages/about.js
+++ intro/src/pages/about.js
@@ -33,6 +33,43 @@ const about = () => {
           />
         </ImageBox>
       </div>
+
+      <div
+        style={{
+          marginTop: "20px",
+        }}
+      >
+        <h3>Fixed</h3>
+        <ImageBox>
+          <StaticImage
+            alt="trap"
+            src="../assets/images/constrained.jpeg"
+            placeholder="tracedSVG"
+            layout="constrained"
+            height={300}
+            className="example-image"
+            as="footer"
+          />
+        </ImageBox>
+      </div>
+
+      <div
+        style={{
+          marginTop: "20px",
+        }}
+      >
+        <h3>FullWidth</h3>
+        <ImageBox>
+          <StaticImage
+            alt="trap"
+            src="../assets/images/constrained.jpeg"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+            className="example-image"
+            as="footer"
+          />
+        </ImageBox>
+      </div>
       <div className="spacer"></div>
     </Layout>
   )
